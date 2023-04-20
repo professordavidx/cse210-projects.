83b762dc246063b459770472099a0f2008b608dd
professordavidx <gom22026@byui.edu>
2023-04-20 14:59:52 -0500

    Added changes

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -10,26 +10,30 @@ class Program
 
         if (number >= 90)
      {
-        Console.WriteLine($"Your final grade is A. Congratulations on passing this course!!");
+        Console.WriteLine("Your final grade is A. Congratulations on passing this course!!");
      }
         else if (number >= 80)
      {
-        Console.WriteLine($"Your final grade is B. Congratulations on passing this course!!");
+        Console.WriteLine("Your final grade is B. Congratulations on passing this course!!");
      }
         else if (number >= 70)
      {
-        Console.WriteLine($"Your final grade is C. Congratulations on passing this course!!");
+        Console.WriteLine("Your final grade is C. Congratulations on passing this course!!");
      }
         else if (number >= 60)
      {
-        Console.WriteLine($"Your final grade is D. You did not approved this course. Keep trying and don't give up!!");
+        Console.WriteLine("Your final grade is D. You did not approved this course. Keep trying and don't give up!!");
      }
 
         else if (number < 60)
      {
-        Console.WriteLine($"Your final grade is F. You did not approved this course. Keep trying and don't give up!!");
+        Console.WriteLine("Your final grade is F. You did not approved this course. Keep trying and don't give up!!");
      }
 
+
+ 
+ 
+ 
     }
 
     
