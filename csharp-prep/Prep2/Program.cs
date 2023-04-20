3d1bd784ec67b1b235407ffd7b84ab5b3052ec09
professordavidx <gom22026@byui.edu>
2023-04-20 14:59:25 -0500

    Added changes

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade: ");
+        string grade = Console.ReadLine();
+        int number =int.Parse(grade);
+
+        if (number >= 90)
+     {
+        Console.WriteLine($"Your final grade is A. Congratulations on passing this course!!");
+     }
+        else if (number >= 80)
+     {
+        Console.WriteLine($"Your final grade is B. Congratulations on passing this course!!");
+     }
+        else if (number >= 70)
+     {
+        Console.WriteLine($"Your final grade is C. Congratulations on passing this course!!");
+     }
+        else if (number >= 60)
+     {
+        Console.WriteLine($"Your final grade is D. You did not approved this course. Keep trying and don't give up!!");
+     }
+
+        else if (number < 60)
+     {
+        Console.WriteLine($"Your final grade is F. You did not approved this course. Keep trying and don't give up!!");
+     }
+
     }
+
+    
 }
