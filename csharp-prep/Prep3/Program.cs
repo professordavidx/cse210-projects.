dd1164f8de34570fa06e113475ee0087f7c4feca
professordavidx <gom22026@byui.edu>
2023-04-25 15:32:53 -0500

    added changes

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicnumber = randomGenerator.Next(1, 101);
+        int guess = -1;
+        int guesscount = 0;
+        
+        while(guess != magicnumber)
+        {
+            Console.Write("Enter magic number: ");
+            guess = int.Parse(Console.ReadLine());
+            guesscount++; 
+
+            if(guess == magicnumber)
+            {
+                Console.Write($"You guessed it in {guesscount} attempts.");
+            }
+            else if(guess > magicnumber)
+            {
+                Console.WriteLine("lower");
+            }
+            else if(guess < magicnumber)
+            {
+                Console.WriteLine("higher");
+            }  
+        }
+
+
+    
+       
+    
+        
+        
+        
+        
+        
+        
+        
+        
+
+
+
+
+        
+        
+
+
+        
+
+
+        
+         
     }
 }
